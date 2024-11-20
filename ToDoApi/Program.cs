d3e7bc4ee9f6e41c55d1c2e9cad88e02fa342f0f
tomasbogalho <tomasbogalho98@gmail.com>
2024-11-20 18:42:24 +0000

    removing one additional ip

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -1,7 +1,3 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using DotNetEnv;
-
 namespace ToDoApi
 {
     public class Program
@@ -23,13 +19,11 @@ namespace ToDoApi
                     // Get backend IP and port from environment variables
                     var backendIp = Environment.GetEnvironmentVariable("BACKEND_IP") ?? "localhost";
                     var backendPort = Environment.GetEnvironmentVariable("BACKEND_PORT") ?? "6003";
-                    var additionalIp1 = Environment.GetEnvironmentVariable("ADDITIONAL_IP1") ?? "10.0.3.4";
-                    var additionalIp2 = Environment.GetEnvironmentVariable("ADDITIONAL_IP2") ?? "10.1.2.4";
+                    var additionalIp = Environment.GetEnvironmentVariable("ADDITIONAL_IP1") ?? "10.0.3.4";
 
                     webBuilder.UseUrls(
                         $"http://{backendIp}:{backendPort}",
-                        $"http://{additionalIp1}:{backendPort}",
-                        $"http://{additionalIp2}:{backendPort}"
+                        $"http://{additionalIp}:{backendPort}"
                     );
                 });
     }
